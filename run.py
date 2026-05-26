f4ee2d623142e610bb6fb7c7ea391811e043e048
gonzalo_lucas_lane <glane@uade.edu.ar>
2026-05-25 22:02:08 -0300

    dashboards cliente y soporte

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import webbrowser
 import time
 import sys
 import os
+import signal
 
 # Ruta al manage.py (relativa desde la raíz del repo)
 MANAGE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backend', 'SIP_GestorTickets')
@@ -19,7 +20,17 @@ time.sleep(2)
 # Abre el navegador en la landing
 webbrowser.open('http://127.0.0.1:8000/')
 
+def cerrar(sig=None, frame=None):
+    print("\nCerrando servidor...")
+    server.terminate()
+    server.wait()
+    sys.exit(0)
+
+# Captura Ctrl+C y cierre de consola
+signal.signal(signal.SIGINT, cerrar)
+signal.signal(signal.SIGTERM, cerrar)
+
 try:
     server.wait()
 except KeyboardInterrupt:
-    server.terminate()
+    cerrar()
