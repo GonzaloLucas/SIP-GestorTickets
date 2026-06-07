0b05fadb2fcd690835c98245b3c64ad3da5f0298
nvera001 <nvera@uade.edu.ar>
2026-06-07 04:24:45 -0300

    Cambio minimo en DJango

--- backend/SIP_GestorTickets/SIP_GestorTickets/settings.py
+++ backend/SIP_GestorTickets/SIP_GestorTickets/settings.py
@@ -139,3 +139,5 @@ EMAIL_HOST_PASSWORD = 'eetn oicn exly amul'
 
 # Esto es lo que va a figurar como remitente en la bandeja de entrada de los clientes
 DEFAULT_FROM_EMAIL = 'Assistech Soporte <assistech.soporte@gmail.com>'
+
+CSRF_TRUSTED_ORIGINS = ['https://assistech.pythonanywhere.com']
--- backend/SIP_GestorTickets/usuarios/admin.py
+++ backend/SIP_GestorTickets/usuarios/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Usuario
+from .models import Usuario, Empresa
 
 
 admin.site.register(Usuario, UserAdmin)
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    # Esto define las columnas que se van a ver en el listado web
+    list_display = ('id_empresa', 'nombre', 'cuil', 'plan', 'fecha_creacion')
+    # Esto te agrega una barra de búsqueda por nombre o CUIL
+    search_fields = ('nombre', 'cuil')
+
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'empresa', 'rol', 'autorizado')
+    list_filter = ('rol', 'autorizado')
+    search_fields = ('email', 'first_name', 'last_name')
